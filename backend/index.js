75c77cc80a52834d4f707d7151040756563032b4
Julian <kounecorp@gmail.com>
2020-09-17 11:38:29 +0200

    feat(backend): add machines

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,62 @@
 const { ApolloServer, gql } = require('apollo-server');
-const db = require("./database");
+// const db = require('./database');
 
 const typeDefs = gql`
-  type Book {
-    title: String
-    author: String
+  type Machine {
+    name: String
+    imgUrl: String
+    description: String
   }
 
   type Query {
-    books: [Book]
+    machines: [Machine]
   }
 `;
 
-const books = [
+const machines = [
   {
-    title: 'Harry Potter and the Chamber of Secrets',
-    author: 'J.K. Rowling',
+    name: 'Dassault Rafale',
+    imgUrl: 'https://www.usinenouvelle.com/mediatheque/9/1/4/000897419_image_896x598/rafale-dassault-aviation.jpg',
+    description:
+      'The Dassault Rafale is a French twin-engine, canard delta wing, multirole fighter aircraft designed and built by Dassault Aviation.',
   },
   {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton',
+    name: 'Ariane 5',
+    imgUrl: 'https://img.20mn.fr/oT8gAsXEQkWZ3i9JSOXGnw/768x492_decollage-fusee-ariane-5-place-deux-satellites-telecommunications-orbite-18-fevrier-2020.jpg',
+    description:
+      'Ariane 5 is a heavy-lift space launch vehicle developed and operated by Arianespace for the European Space Agency (ESA).',
+  },
+  {
+    name: 'School bus',
+    imgUrl: 'https://www.vmcdn.ca/f/files/moosejawtoday/images/transportation/school-bus-stock-photo.jpg;w=1000;h=662;mode=crop',
+    description:
+      'A school bus is a type of bus owned, leased, contracted to, or operated by a school or school district.',
+  },
+  {
+    name: 'Motorized scooter',
+    imgUrl: 'https://cdn.shopify.com/s/files/1/0030/8183/0512/products/apollo-explore-electric-scooter-665723_2048x2048.jpg?v=1596138026',
+    description:
+      'A motorized scooter is a powered stand-up scooter using a small utility internal combustion engine or, more commonly, an electric motor.',
+  },
+  {
+    name: 'Garbage truck',
+    imgUrl: 'https://www.revell.de/out/pictures/master/product/1/00808_mdpw_garbage_truck.jpg',
+    description:
+      'A garbage truck is a truck specially designed to collect municipal solid waste and transport it to a solid waste treatment facility, such as a landfill or transfer station.',
   },
 ];
 
-db.startDatabase();
+// This doesn't need DB for now
+// db.startDatabase();
 
 const resolvers = {
   Query: {
-    books: () => books,
+    machines: () => machines,
   },
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+  console.log(`🚀  VROOOUM at ${url}`);
 });
